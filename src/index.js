fb5a07a422454417cf41b431ac20c7a09493d7b0
MAHESHGONDA <maheshmalknoor@gmail.com>
2021-03-12 13:08:23 +0530

    Book app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
-function Greeting() {
-  return <h1>Hello World</h1>
+import './index.css'
+function Booklist() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+function Book() {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => {
+  return (
+    <img
+      src='https://images-eu.ssl-images-amazon.com/images/I/61ibgnNTzDL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg'
+      alt='book'
+    />
+  )
+}
+const Title = () => <h1>How To Stop Worrying & Start Living</h1>
+
+const Author = () => <h4>Dale Carnegie</h4>
+ReactDom.render(<Booklist />, document.getElementById('root'))
