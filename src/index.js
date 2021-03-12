da6b2a1c044c846a9af9affaea106393125239f4
MAHESHGONDA <maheshmalknoor@gmail.com>
2021-03-12 14:09:56 +0530

    props children example

--- src/index.js
+++ src/index.js
@@ -1,36 +1,59 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
-function Booklist() {
+
+// set up
+const firstBok = {
+  img:
+    'https://images-eu.ssl-images-amazon.com/images/I/61ibgnNTzDL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
+  imgAlt: 'book',
+  title: 'How To Stop Worrying & Start Living',
+  authorName: 'Dale Carnegie',
+}
+
+const secondBok = {
+  img:
+    'https://m.media-amazon.com/images/I/71VSswnjh9L._AC_UL480_FMwebp_QL65_.jpg',
+  imgAlt: 'book',
+  title: '21 Lessons for the 21st Century',
+  authorName: 'Taschenbuch',
+}
+
+// Booklist component
+const Booklist = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBok.img}
+        alt={firstBok.imgAlt}
+        title={firstBok.title}
+        authorName={firstBok.authorName}
+      >
+        <p>Helloo i am children </p>
+      </Book>
+      <Book
+        img={secondBok.img}
+        alt={secondBok.imgAlt}
+        title={secondBok.title}
+        authorName={firstBok.authorName}
+      />
     </section>
   )
 }
 
-function Book() {
+//book component
+const Book = (props) => {
+  console.log(props)
+  //object destructuring
+  const { img, imgAlt, title, authorName, children } = props
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={imgAlt} />
+      <h1>{title}</h1>
+      <h4>{authorName}</h4>
+      {children}
     </article>
   )
 }
 
-const Image = () => {
-  return (
-    <img
-      src='https://images-eu.ssl-images-amazon.com/images/I/61ibgnNTzDL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg'
-      alt='book'
-    />
-  )
-}
-const Title = () => <h1>How To Stop Worrying & Start Living</h1>
-
-const Author = () => <h4>Dale Carnegie</h4>
 ReactDom.render(<Booklist />, document.getElementById('root'))
