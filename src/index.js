98bfe36125319d6db9020fc5eab0c6c4c26568f0
MAHESHGONDA <maheshmalknoor@gmail.com>
2021-03-15 15:09:27 +0530

    added devcontainers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css'
 // set up
 const books = [
   {
+    id: 1,
     img:
       'https://images-eu.ssl-images-amazon.com/images/I/61ibgnNTzDL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
     imgAlt: 'book',
@@ -12,11 +13,28 @@ const books = [
     authorName: 'Dale Carnegie',
   },
   {
+    id: 2,
     img:
-      'https://m.media-amazon.com/images/I/71VSswnjh9L._AC_UL480_FMwebp_QL65_.jpg',
+      'https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
     imgAlt: 'book',
-    title: '21 Lessons for the 21st Century',
-    authorName: 'Taschenbuch',
+    title: 'The Psychology of Money',
+    authorName: 'Morgan Housel',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
+    imgAlt: 'book',
+    title: 'Karma: A Yogis Guide to Crafting Your Destiny',
+    authorName: 'Sadhguru',
+  },
+  {
+    id: 4,
+    img:
+      'https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg',
+    imgAlt: 'book',
+    title: 'The Alchemist',
+    authorName: 'Paulo Coelho',
   },
 ]
 
@@ -24,20 +42,10 @@ const books = [
 const Booklist = () => {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBok.img}
-        alt={firstBok.imgAlt}
-        title={firstBok.title}
-        authorName={firstBok.authorName}
-      >
-        <p>Helloo i am children </p>
-      </Book>
-      <Book
-        img={secondBok.img}
-        alt={secondBok.imgAlt}
-        title={secondBok.title}
-        authorName={firstBok.authorName}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />
+        // return <Book key={book.id} book={book} />
+      })}
     </section>
   )
 }
@@ -46,13 +54,12 @@ const Booklist = () => {
 const Book = (props) => {
   console.log(props)
   //object destructuring
-  const { img, imgAlt, title, authorName, children } = props
+  const { img, imgAlt, title, authorName } = props
   return (
     <article className='book'>
       <img src={img} alt={imgAlt} />
       <h1>{title}</h1>
       <h4>{authorName}</h4>
-      {children}
     </article>
   )
 }
